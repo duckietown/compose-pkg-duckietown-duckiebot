b4b628fe5a2a3cdb264cee117d8db3d511de72d2
Andrea F. Daniele <afdaniele@ttic.edu>
2021-01-21 11:15:40 -0600

    Duckiebot class is now using files-api to read/write from/to the robot

--- Duckiebot.php
+++ Duckiebot.php
@@ -7,9 +7,7 @@
 namespace system\packages\duckietown_duckiebot;
 
 use \system\classes\Core;
-use \system\classes\Utils;
-use \system\classes\Database;
-use \system\classes\Configuration;
+
 
 /**
  *   Module for managing Duckiebots
@@ -17,18 +15,23 @@ use \system\classes\Configuration;
 class Duckiebot {
     
     private static $initialized = false;
-    private static $PERMISSIONS_KEYS = [
+    private static $files_api = null;
+    private static $PERMISSION_LOCATION = 'config/permissions/%s';
+    private static $PERMISSION_KEYS = [
         "allow_push_logs_data",
         "allow_push_stats_data",
         "allow_push_config_data",
     ];
-    private static $PERMISSION_FILENAME = '/data/config/permissions/%s';
-    private static $SETTING_FILENAME = [
-        "type" => "/data/config/robot_type",
-        "configuration" => "/data/config/robot_configuration",
-        "hardware" => "/data/config/robot_hardware",
-        "autolab/tag_id" => "/data/config/autolab/tag_id",
-        "autolab/map_name" => "/data/config/autolab/map_name"
+    private static $CONFIGURATION_LOCATION = 'config/robot_%s';
+    private static $CONFIGURATION_KEYS = [
+        "type",
+        "configuration",
+        "hardware"
+    ];
+    private static $AUTOLAB_CFG_LOCATION = 'config/autolab/%s';
+    private static $AUTOLAB_CFG_KEYS = [
+        "tag_id",
+        "map_name"
     ];
     
     // disable the constructor
@@ -46,9 +49,9 @@ class Duckiebot {
      *        where, the `success` field indicates whether the function succeded.
      *        The `data` field contains errors when `success` is `FALSE`.
      */
-    public static function init() {
+    public static function init(): array {
         if (!self::$initialized) {
-            // do stuff
+            self::$files_api = new FilesAPI();
             //
             self::$initialized = true;
             return ['success' => true, 'data' => null];
@@ -62,7 +65,7 @@ class Duckiebot {
      * @retval boolean
      *        whether the module is initialized.
      */
-    public static function isInitialized() {
+    public static function isInitialized(): bool {
         return self::$initialized;
     }//isInitialized
     
@@ -77,7 +80,7 @@ class Duckiebot {
      *        where, the `success` field indicates whether the function succeded.
      *        The `data` field contains errors when `success` is `FALSE`.
      */
-    public static function close() {
+    public static function close(): array {
         // do stuff
         return ['success' => true, 'data' => null];
     }//close
@@ -86,19 +89,17 @@ class Duckiebot {
     // =======================================================================================================
     // Public functions
     
-    public static function getDuckiebotName() {
+    public static function getDuckiebotName(): string {
         $duckiebot_hostname = self::getDuckiebotHostname();
         // remove '.local' from the end of the host string (if present)
-        $duckiebot_name = preg_replace('/\.local$/', '', $duckiebot_hostname);
-        // ---
-        return $duckiebot_name;
+        return preg_replace('/\.local$/', '', $duckiebot_hostname);
     }//getDuckiebotName
     
-    public static function getDuckiebotType($duckiebot_hostname) {
-        return self::getFileFromRobot('/config/robot_type', $duckiebot_hostname);
+    public static function getDuckiebotType(): string {
+        return self::$files_api->get('/config/robot_type');
     }//getDuckiebotType
     
-    public static function getDuckiebotHostname() {
+    public static function getDuckiebotHostname(): string {
         $duckiebot_name = Core::getSetting('duckiebot_name', 'duckietown_duckiebot');
         // revert to http host if no vehicle name is set
         if (strlen($duckiebot_name) < 2) {
@@ -113,19 +114,86 @@ class Duckiebot {
         return $duckiebot_hostname;
     }//getDuckiebotHostname
     
-    public static function setDuckiebotPermission($key, $value) {
-        if (!in_array($key, self::$PERMISSIONS_KEYS))
+    public static function getDuckiebotPermission($key): array {
+        if (!in_array($key, self::$PERMISSION_KEYS))
             return ['success' => false, 'data' => "Permission key `$key` not recognized."];
-        $fpath = sprintf(self::$PERMISSION_FILENAME, $key);
-        return self::writeFileToDisk($fpath, trim($value));
+        $fpath = sprintf(self::$PERMISSION_LOCATION, $key);
+        return self::$files_api->get($fpath);
+    }//getDuckiebotPermission
+    
+    public static function getDuckiebotConfiguration($key): array {
+        if (!in_array($key, self::$CONFIGURATION_KEYS))
+            return ['success' => false, 'data' => "Configuration key `$key` not recognized."];
+        $fpath = sprintf(self::$CONFIGURATION_LOCATION, $key);
+        return self::$files_api->get($fpath);
+    }//getDuckiebotConfiguration
+    
+    public static function getAutolabConfiguration($key): array {
+        if (!in_array($key, self::$AUTOLAB_CFG_KEYS))
+            return ['success' => false, 'data' => "Autolab setting key `$key` not recognized."];
+        $fpath = sprintf(self::$AUTOLAB_CFG_LOCATION, $key);
+        return self::$files_api->get($fpath);
+    }//getAutolabConfiguration
+    
+    public static function setDuckiebotPermission($key, $value): array {
+        if (!in_array($key, self::$PERMISSION_KEYS))
+            return ['success' => false, 'data' => "Permission key `$key` not recognized."];
+        $fpath = sprintf(self::$PERMISSION_LOCATION, $key);
+        return self::$files_api->post($fpath, is_bool($value)? +$value : trim($value));
     }//setDuckiebotPermission
     
-    public static function setDuckiebotConfiguration($key, $value) {
-        if (!array_key_exists($key, self::$SETTING_FILENAME))
-            return ['success' => false, 'data' => "Setting `$key` not recognized."];
-        $fpath = self::$SETTING_FILENAME[$key];
-        return self::writeFileToDisk($fpath, trim($value));
+    public static function setDuckiebotConfiguration($key, $value): array {
+        if (!in_array($key, self::$CONFIGURATION_KEYS))
+            return ['success' => false, 'data' => "Configuration key `$key` not recognized."];
+        $fpath = sprintf(self::$CONFIGURATION_LOCATION, $key);
+        return self::$files_api->post($fpath, trim($value));
     }//setDuckiebotConfiguration
+    
+    public static function setAutolabConfiguration($key, $value): array {
+        if (!in_array($key, self::$AUTOLAB_CFG_KEYS))
+            return ['success' => false, 'data' => "Autolab setting key `$key` not recognized."];
+        $fpath = sprintf(self::$AUTOLAB_CFG_LOCATION, $key);
+        return self::$files_api->post($fpath, trim($value));
+    }//setAutolabConfiguration
+    
+    public static function getDuckiebotPermissions(): array {
+        $out = [
+            "success" => true,
+            "data" => []
+        ];
+        foreach (self::$PERMISSION_KEYS as $key) {
+            $res = self::getDuckiebotPermission($key);
+            if (!$res['success']) {
+                return $res;
+            }
+            $out['data'][$key] = $res['data'];
+        }
+        return $out;
+    }//getDuckiebotPermissions
+    
+    public static function getDuckiebotConfigurations(): array {
+        $out = [];
+        foreach (self::$CONFIGURATION_KEYS as $key) {
+            $res = self::getDuckiebotConfiguration($key);
+            if (!$res['success']) {
+                return $res;
+            }
+            $out[$key] = $res['data'];
+        }
+        return $out;
+    }//getDuckiebotConfigurations
+    
+    public static function getAutolabConfigurations(): array {
+        $out = [];
+        foreach (self::$AUTOLAB_CFG_KEYS as $key) {
+            $res = self::getAutolabConfiguration($key);
+            if (!$res['success']) {
+                return $res;
+            }
+            $out[$key] = $res['data'];
+        }
+        return $out;
+    }//getAutolabConfigurations
     
     
     // =======================================================================================================
@@ -133,29 +201,7 @@ class Duckiebot {
     
     // YOUR PRIVATE METHODS HERE
     
-    
-    private static function getFileFromRobot($filepath, $duckiebot_hostname = null) {
-        $files_api_hostname = $duckiebot_hostname;
-        if (is_null($duckiebot_hostname)) {
-            $files_api_hostname = Core::getSetting('files_api_host', 'duckietown_duckiebot');
-        }
-        $files_api_port = Core::getSetting('files_api_port', 'duckietown_duckiebot');
-        // revert to duckiebot hostname (or eventually to http host) if no vehicle name is set
-        if (strlen($files_api_hostname) < 2) {
-            $files_api_hostname = self::getDuckiebotHostname();
-        }
-        // prepare url
-        $url = sprintf('http://%s:%s/%s', $files_api_hostname, $files_api_port, ltrim($filepath, '/'));
-        // fetch data from robot
-        if (!$data = file_get_contents($url)) {
-            $error = error_get_last();
-            Core::throwError(sprintf("HTTP request failed. Error was: %s", $error['message']));
-        } else {
-            return $data;
-        }
-    }//getFileFromRobot
-    
-    private static function writeFileToDisk($fpath, $content) {
+    private static function writeFileToDisk($fpath, $content): array {
         $fdirpath = dirname($fpath);
         if (file_exists($fdirpath) && !is_writable($fdirpath)) {
             return ['success' => false, 'data' => "Directory `$fdirpath` cannot be written."];
@@ -178,4 +224,73 @@ class Duckiebot {
     }//writeFileToDisk
     
 }//Duckiebot
+
+
+
+class FilesAPI {
+    
+    /**
+     * @var string
+     */
+    private $url;
+    
+    public function __construct($hostname = null) {
+        $files_api_hostname = $hostname;
+        if (is_null($hostname)) {
+            $files_api_hostname = Core::getSetting('files_api/hostname', 'duckietown_duckiebot');
+        }
+        // revert to duckiebot hostname (or eventually to http host) if no vehicle name is set
+        if (strlen($files_api_hostname) < 2) {
+            $files_api_hostname = Duckiebot::getDuckiebotHostname();
+        }
+        // prepare url
+        $this->url = sprintf('http://%s/files/', $files_api_hostname);
+    }
+    
+    public function get($fpath, $format=null): array {
+        $qs = [];
+        if (!is_null($format))
+            $qs['format'] = $format;
+        $url = self::url($fpath, $qs);
+        // fetch data from robot
+        $data = file_get_contents($url);
+        if ($data === false) {
+            $error = error_get_last();
+            return ['success' => false, 'data' => $error['message']];
+        } else {
+            return ['success' => true, 'data' => $data];
+        }
+    }
+    
+    public function post($fpath, $content, $format=null): array {
+        $qs = [];
+        if (!is_null($format))
+            $qs['format'] = $format;
+        $url = self::url($fpath, $qs);
+        // send data to robot
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
+        $res = curl_exec($ch);
+        curl_close($ch);
+        // check if we got anything
+        if ($res === false) {
+            $ch_info = curl_getinfo($ch);
+            return [
+                'success' => false,
+                'data' => sprintf('The Files API returned the code `%s`', $ch_info['http_code'])
+            ];
+        }
+        // looks like we got something
+        return ['success' => true, 'data' => $res];
+    }
+    
+    private function url($resource, $qs = []): string {
+        return join_path($this->url, $resource, toQueryString(array_keys($qs), $qs, true));
+    }
+    
+}
 ?>
