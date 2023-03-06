ab21cd177f84b3c9d736a463db79d57c486726a2
Jason, HU Shengjie <jasonhu515@gmail.com>
2023-03-06 16:00:09 +0100

    Fetch tests from health API, render in modal

--- pages/robot/tabs/components/index.php
+++ pages/robot/tabs/components/index.php
@@ -1,9 +1,20 @@
 <?php
 use \system\classes\Core;
+use \system\packages\ros\ROS;
 use \system\packages\duckietown_duckiebot\Duckiebot;
 
 
 $dbot_hostname = Duckiebot::getDuckiebotHostname();
+
+$robot_name = Duckiebot::getDuckiebotName();
+$robot_type = Duckiebot::getRobotType();
+$ros_hostname = ROS::sanitize_hostname($dbot_hostname);
+
+$connected_evt = ROS::get_event(ROS::$ROSBRIDGE_CONNECTED, $ros_hostname);
+$error_evt = ROS::get_event(ROS::$ROSBRIDGE_ERROR, $ros_hostname);
+$closed_evt = ROS::get_event(ROS::$ROSBRIDGE_CLOSED, $ros_hostname);
+
+ROS::connect($ros_hostname);
 ?>
 
 <style type="text/css">
@@ -131,6 +142,23 @@ $dbot_hostname = Duckiebot::getDuckiebotHostname();
     }
 </style>
 
+<table style="width: 970px; margin: auto; margin-bottom: 12px">
+    <tr>
+        <td class="text-left" style="width:33%">
+            <i class="fa fa-car" aria-hidden="true"></i> Vehicle:
+            <strong><?php echo $robot_name ?></strong>
+        </td>
+        <td class="text-center"
+            style="width:33%; border-left: 1px solid lightgrey; border-right: 1px solid lightgrey">
+            Components
+        </td>
+        <td class="text-center" style="width:33%; text-align: right">
+        <span id="vehicle_bridge_status">
+          <i class="fa fa-spinner fa-pulse"></i> Connecting...
+        </span>
+        </td>
+    </tr>
+</table>
 
 <div id="_placeholder_img">
     <img src="<?php echo Core::getImageURL('loading_blue.gif') ?>" alt=""/>
@@ -142,6 +170,28 @@ $dbot_hostname = Duckiebot::getDuckiebotHostname();
 
 
 <script type="text/javascript">
+
+    $(document).on("<?php echo $connected_evt ?>", function (evt) {
+        console.log('Connected to websocket server.');
+        $('#vehicle_bridge_status').html(
+            '<span class="glyphicon glyphicon-ok-sign" aria-hidden="true" style="color:green"></span> Bridge: <strong>Connected</strong>'
+        );
+
+    });
+
+    $(document).on("<?php echo $error_evt ?>", function (evt, error) {
+        console.log('Error connecting to websocket server: ', error);
+        $('#vehicle_bridge_status').html(
+            '<span class="glyphicon glyphicon-remove-sign" aria-hidden="true" style="color:red"></span> Bridge: <strong>Error</strong>'
+        );
+    });
+
+    $(document).on("<?php echo $closed_evt ?>", function (evt) {
+        console.log('Connection to websocket server closed.');
+        $('#vehicle_bridge_status').html(
+            '<span class="glyphicon glyphicon-off" aria-hidden="true" style="color:red"></span> Bridge: <strong>Closed</strong>'
+        );
+    });
     
     let _api_url = "http://<?php echo $dbot_hostname ?>/{api}/{action}/{resource}";
     window.ROBOT_COMPONENT_TYPE_TO_ICON = {
@@ -153,7 +203,6 @@ $dbot_hostname = Duckiebot::getDuckiebotHostname();
         "TOF": "eye",
         "MOTOR": "car",
         "BATTERY": "battery",
-        "FLIGHT_CONTROLLER": "plane",
     };
     window.ROBOT_COMPONENT_DEFAULT_ICON = "square";
     
@@ -175,6 +224,9 @@ $dbot_hostname = Duckiebot::getDuckiebotHostname();
                         <td class="_robot_component_info">
                             <h4 class="text-left">{name}</h4>
                             <h6 class="text-left">{description}</h6>
+                        </td>
+                        <td>
+                            {verification_test_button}
                         </td>
                         <td rowspan="2" class="_robot_component_stats">
                             <h5><strong>Supported:</strong> {supported}</h5>
@@ -270,6 +322,106 @@ $dbot_hostname = Duckiebot::getDuckiebotHostname();
                 component.calibration.needed?
                     "<h5><strong>Calibrated:</strong> {0}</h5>".format(status_icon(component.calibration.completed)) : ''
             ) : '';
+            let verification_test_button = "";
+            if (component.detected) {
+                let id_str_name = name.replaceAll(' ', '-');
+                verification_test_button = '<button type="button" class="btn btn-info" data-toggle="modal" data-target="' + '#modal-' + id_str_name + '">Verify Hardware</button>';
+
+                let test_modal = `
+                    <!-- Modal -->
+                    <div class="modal fade" id="{modal_id}" role="dialog">
+                        <div class="modal-dialog modal-lg">
+                        
+                        <!-- Modal content-->
+                        <div class="modal-content">
+                            <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title">{test_name}</h4>
+                            </div>
+                            <div class="modal-body">
+                            <p id="{desc_id}" class="text-left"></p>
+                            </div>
+                            <div class="modal-footer">
+                            <p class="text-left" id="{output_id}"></p>
+                            <div id="{prog_id}" class="progress" style="display:none">
+                                <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+                                    <span class="sr-only">Running</span>
+                                </div>
+                            </div>
+                            <button type="button" class="btn btn-primary text-left" id="{button_id}">Run test</button>
+                            <!-- button type="button" class="btn btn-default" data-dismiss="modal">Close</button -->
+                            </div>
+                            <div class="modal-footer">
+                            <button type="button" class="btn btn-success">Success</button>
+                            <button type="button" class="btn btn-danger">Failed</button>
+                            <!-- button type="button" class="btn btn-default" data-dismiss="modal">Close</button -->
+                            </div>
+                        </div>
+                        
+                        </div>
+                    </div>
+                `;
+                container_div.append(test_modal.format({
+                    modal_id: 'modal-' + id_str_name,
+                    test_name: "Verification: " + name,
+                    button_id: 'btn-' + id_str_name,
+                    output_id: 'out-' + id_str_name,
+                    prog_id: 'prog-' + id_str_name,
+                    desc_id: 'desc-' + id_str_name,
+                }));
+
+                let desc_id = "#desc-" + id_str_name;
+                // test ros service description
+                let _testDescClient = new ROSLIB.Service({
+                    ros : window.ros['<?php echo $dbot_hostname ?>'],
+                    name : '/' + '<?php echo $robot_name?>' + '/' + component.test_service_name + '/desc',
+                    serviceType : 'std_srvs/Trigger'
+                });
+                var reqDesc = new ROSLIB.ServiceRequest({});
+                _testDescClient.callService(reqDesc, function(result) {
+                    console.log('Desc service call on \n'
+                    + _testDescClient.name
+                    + ':\n'
+                    + result.message);
+                    $(desc_id).html(result.message.replaceAll("\n", "<br>"));
+                });
+
+                // test ros service
+                let _testRunClient = new ROSLIB.Service({
+                    ros : window.ros['<?php echo $dbot_hostname ?>'],
+                    name : '/' + '<?php echo $robot_name?>' + '/' + component.test_service_name + '/run',
+                    serviceType : 'std_srvs/Trigger'
+                });
+                let request = new ROSLIB.ServiceRequest({});
+
+                let btn_id = "#btn-" + id_str_name;
+                let out_id = "#out-" + id_str_name;
+                let prog_id = "#prog-" + id_str_name;
+
+                $(btn_id).click(function() {
+                    console.log("Triggered " + id_str_name);
+
+                    // clear output
+                    $(out_id).html("");
+                    // show progress
+                    $(prog_id).show();
+                    // hide button
+                    $(btn_id).hide();
+
+                    // console.log($dbot_hostname)
+                    _testRunClient.callService(request, function(result) {
+                        $(prog_id).hide();
+                        $(btn_id).show();
+                        console.log('Result for service call on \n'
+                        + _testRunClient.name
+                        + ':\n'
+                        + result.message);
+
+                        $(out_id).html(result.message.replaceAll("\n", "<br>"));
+                    });
+                })
+
+            }
             // create component's nav
             div.html(
                 _nav.format({
@@ -279,7 +431,8 @@ $dbot_hostname = Duckiebot::getDuckiebotHostname();
                     bus: bus,
                     supported: supported,
                     detected: detected,
-                    calibrated: calibrated
+                    calibrated: calibrated,
+                    verification_test_button: verification_test_button,
                 })
             );
             div.find('nav').css('display', 'inherit');
